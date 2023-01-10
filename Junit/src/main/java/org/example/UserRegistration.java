88e3462504ab3d6884e8c98144ca605ebfe602ac
Darshan Patil <iampatildarsh>
2023-01-10 15:34:16 +0530

    Validation of Mobile Number

--- Junit/src/main/java/org/example/UserRegistration.java
+++ Junit/src/main/java/org/example/UserRegistration.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
+    private static final String MOBILE_NUMBER_PATTERN = "^[9,1]{2}?[0-9]{10}$";
 
     public boolean validateFirstName(String firstName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -19,6 +20,11 @@ public class UserRegistration {
     public boolean validateEmailID(String emailID) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         return pattern.matches(EMAIL_PATTERN, emailID);
+    }
+
+    public boolean validateMobileNumber(String mobileNumber) {
+        Pattern pattern = Pattern.compile(MOBILE_NUMBER_PATTERN);
+        return pattern.matches(MOBILE_NUMBER_PATTERN, mobileNumber);
     }
 }
 
